ea414564d157fdab3c33a0b637ae143c11504430
Andrew Ligon <andrew.ligon@nextthought.com>
2020-08-29 15:53:12 -0500

    start styling the body editor

--- src/editor/parts/content/Body.jsx
+++ src/editor/parts/content/Body.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+import {Editor} from '@nti/web-editor';
 
-export default function BodyEditor () {
+import Styles from './Styles.css';
+
+const cx = classnames.bind(Styles);
+
+BodyEditor.propTypes = {
+	className: PropTypes.string,
+	error: PropTypes.any
+};
+export default function BodyEditor ({className, error, ...otherProps}) {
 	return (
-		<div>
-			Body
+		<div className={cx('body', className)}>
+			<Editor {...otherProps} />
 		</div>
 	);
 }
