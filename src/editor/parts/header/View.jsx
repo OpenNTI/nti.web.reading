46ccad8b4bb97d6beb3123c966063b5927e4f784
Andrew Ligon <andrew.ligon@nextthought.com>
2020-09-18 16:13:11 -0500

    style the breadcrumb and pager in the header

--- src/editor/parts/header/View.jsx
+++ src/editor/parts/header/View.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import {Pager, Text} from '@nti/web-commons';
 
-import Styles from '../Styles.css';
 import {Locations} from '../Constants';
+
+import Styles from './Styles.css';
 
 const cx = classnames.bind(Styles);
 
 ReadingEditorHeader.Location = Locations.Header;
 ReadingEditorHeader.propTypes = {
-	children: PropTypes.any
+	children: PropTypes.any,
+	breadcrumb: PropTypes.arrayOf(
+		PropTypes.shape({
+			label: PropTypes.string,
+			handleRoute: PropTypes.func
+		})
+	),
+	pageSource: PropTypes.any
 };
-export default function ReadingEditorHeader ({children}) {
+export default function ReadingEditorHeader ({children, breadcrumb, pageSource}) {
 	return (
 		<div className={cx('header')}>
 			<div className={cx('bar')}>
-
+				{breadcrumb && (
+					<ul className={cx('breadcrumb')}>
+						{breadcrumb.map((item, index) => (
+							<Text.Base as ="li" key={index} onClick={item.handleRoute}>
+								{item.label}
+							</Text.Base>
+						))}
+					</ul>
+				)}
+				{pageSource && (<Pager pageSource={pageSource} />)}
 			</div>
 			<div className={cx('controls')}>
 				{children}
