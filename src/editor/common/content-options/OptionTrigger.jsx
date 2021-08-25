1ce6a510599ddf2b2fca41d38bce5f8014577bc6
Andrew Ligon <aligon90@gmail.com>
2021-08-24 22:14:41 -0500

    update button import

--- src/editor/common/content-options/OptionTrigger.jsx
+++ src/editor/common/content-options/OptionTrigger.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 
 import { scoped } from '@nti/lib-locale';
-import { Button, Text, Icons } from '@nti/web-commons';
+import { Text, Icons } from '@nti/web-commons';
+import { Button } from "@nti/web-core";
 
 import Styles from './OptionTrigger.css';
 import Context from './Context';
--- src/editor/custom-blocks/custom-block/Controls.jsx
+++ src/editor/custom-blocks/custom-block/Controls.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 
-import { Button, Icons, Text } from '@nti/web-commons';
+import { Icons, Text } from '@nti/web-commons';
+import { Button } from "@nti/web-core";
 
 import Styles from './Styles.css';
 
